74b7906c94e5870e1a365c3b466e8041a9653656
jesusLPZP <jesusl012345678900000@gmail.com>
2025-10-08 13:32:00 -0600

    Create Calculating_age.py

---
+++ Calculating_age.py
@@ -0,0 +1,9 @@
+year_of_birth = "2007"
+Age = "18"
+first_name = "Jesus"
+Favorite_Hobby = "longboard"
+txt = f"My name is {first_name}, I was born in the year{year}"
+
+print(txt)
+
+print(f"I, {first_name}, also like to {Favorite_Hobby} during my freetime.")
