3f55f67aac16a24cc1e76ea98a5614ab3f89a2f2
jesusLPZP <jesusl012345678900000@gmail.com>
2025-10-13 13:20:19 -0600

    finishing the last touches for class credit

--- calculating_peoples_age.py
+++ calculating_peoples_age.py
@@ -1,7 +1,17 @@
-# These codes will store the name of the user and greet them
+# This code will store the name of the user and greet them
 name = input("Enter your name:")
-print("hello",name)
 
-# These codes will store the age of the user as int.
-age = int(input("Enter your age:"))
-print("You are",age,"years old")
+#This code will store the users favorite hobby
+favorite_hobby = input("Enter your favorite hobby:")
+
+# This code will store the current year the user types in.
+current_year = int(input("Enter the current year:"))
+
+# This code will store the user's year of birth.
+year_of_birth = int(input("Enter your year of birth:"))
+
+# This code will calculate the user's current age.
+current_age = current_year - year_of_birth
+
+#This code will print out the user's name, age, and favorite hobby.
+print(f"I, {name}, I am {current_age} and I spend my free time by {favorite_hobby} ")
