d63befda059d1393210c883e3b9121650fce4435
jesusLPZP <jesusl012345678900000@gmail.com>
2025-10-13 13:14:10 -0600

    trying to add more codes

---
+++ calculating_peoples_age.py
@@ -0,0 +1,7 @@
+# These codes will store the name of the user and greet them
+name = input("Enter your name:")
+print("hello",name)
+
+# These codes will store the age of the user as int.
+age = int(input("Enter your age:"))
+print("You are",age,"years old")
