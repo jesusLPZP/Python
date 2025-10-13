173fc4ec4205e5bdf88cf978048b1efff800c8dc
jesusLPZP <jesusl012345678900000@gmail.com>
2025-10-13 12:38:55 -0600

    example used in class

---
+++ Python_ex.py
@@ -0,0 +1,12 @@
+shape = "Circle"
+PI = 3.14159
+radius = 20
+print(f"Variable shape data type is: {type(shape)}")
+print(f"Constant PI  data type is: {type(PI)}")
+print(f"Variable radius data type is: {type(radius)}")
+area = radius * radius * PI
+print(f"The area for a circle of radius {radius} is {area}")
+radius = 33.4
+print(f"Variable radius data type is: {type(radius)}")
+area = radius * radius * PI
+print(f"The area for a circle of radius {radius} is {area}")
