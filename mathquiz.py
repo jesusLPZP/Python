9ee0389660d2fae605c03e45ede84e754b852b7a
jesusLPZP <jesusl012345678900000@gmail.com>
2025-10-20 11:13:44 -0600

    added some more comments for the lines of code.

--- mathquiz.py
+++ mathquiz.py
@@ -2,15 +2,18 @@ import random
 # 1. Generate two random single-digit integers (0–9)
 number1 = random.randint(0, 9)
 number2 = random.randint(0, 9)
+#chooses a random number and are listed as special variables 
 
 # 2. If number1 < number2, swap number1 with number2
 if number1 < number2:
    temp = number1
    number1 = number2
    number2 = temp
+#swaps the numbers to avoid negative answers if the second number is larger than the first
 
 # 3. Prompt the student to answer "What is number1 - number2?"
 answer = int(input(f"What is {number1} - {number2}? "))
+#takes the user's answer as an integer input
 
 # 4. Grade the answer and display the result
 if number1 - number2 == answer:
