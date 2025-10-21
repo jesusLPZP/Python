77e6ef92a22af7d3890852f75b905401eb3cb605
jesusLPZP <jesusl012345678900000@gmail.com>
2025-10-21 09:14:46 -0600

    added some more text to show the player lost

--- RPS.py
+++ RPS.py
@@ -3,15 +3,20 @@ rock = 1
 paper = 2
 scissors = 3
 player1_choice = int(input("Player 1,Enter 1 for Rock, 2 for Paper, or 3 for Scissors: "))
-player2_choice = int(input("Player 2 ,Enter 1 for Rock, 2 for Paper, or 3 for Scissors: "))
+player2_choice = int(input("Player 2,Enter 1 for Rock, 2 for Paper, or 3 for Scissors: "))
 if player1_choice == player2_choice:
-    print("It's a tie!")
+    print("It's a tie! Both players chose", str(player1_choice))
 elif (player1_choice == rock and player2_choice == scissors):
     print ("You win! Rock crushes Scissors.")
 elif (player1_choice == paper and player2_choice == rock):
     print("You win! Paper covers Rock.")
 elif (player1_choice == scissors and player2_choice == paper):
     print("You win! Scissors cut Paper.")
-else:
-    print("player 2 wins!")
+elif (player2_choice == rock and player1_choice == scissors):
+    print("Player 2 wins! Rock crushes Scissors.")
+elif (player2_choice == paper and player1_choice == rock):
+    print("Player 2 wins! Paper covers Rock.")
+else: 
+    (player2_choice == scissors and player1_choice == paper)
+    print("Player 2 wins! Scissors cut Paper.")
 
