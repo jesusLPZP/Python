1293b120f78b59b8528a8712f05bd8bbb25ab3ae
jesusLPZP <jesusl012345678900000@gmail.com>
2025-10-22 13:46:38 -0600

    grammar check

--- continuelab.py
+++ continuelab.py
@@ -7,5 +7,5 @@ while user == "y" or user == "Y":
     runs += 1
     print("Do you want to continue? (y/n): ")
     user = input()
-#prints the amount of thimes the user started the program again before typing n or anything else other then "y" or ""Y" 
-print("You chose to not continue,you played the program " +str(runs)+ " times.")
+#prints the amount of times the user started the program again before typing n or anything else other than "y" or ""Y" 
+print("You chose not to continue,you played the program " +str(runs)+ " times.")
